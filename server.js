2481961acd51ca7c56aa7d583db501e9eb3c0f3c
sasha <ssm9960@nyu.edu>
2022-11-22 00:16:30 +0400

    .

--- server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.use('/', express.static('public'));
-
-
-app.listen(3000, ( ) => {
-    console.log("app is listening at localhost:3000");
-});
